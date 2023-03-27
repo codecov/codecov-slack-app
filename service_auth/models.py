28556829551ab0cb7299a30c72e69d4cac36e85a
Rula Abu Hasna <rula@codecov.io>
2023-03-27 16:36:53 +0300

    adjust names

--- service_auth/models.py
+++ service_auth/models.py
@@ -41,20 +41,19 @@ class Service(models.Model):
     user = models.ForeignKey(
         SlackUser, on_delete=models.CASCADE, related_name="services"
     )
-    service_name = models.CharField(
-        max_length=50, choices=ServiceOptions.choices
-    )
-    service_id = models.CharField(max_length=50, unique=True)
+    service = models.CharField(max_length=50, choices=ServiceOptions.choices)
+    service_userid = models.CharField(max_length=50, unique=True)
+    service_username = models.CharField(max_length=100, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.service_name
+        return self.service
 
     class Meta:
         indexes = [
-            models.Index(fields=["service_name", "service_id"]),
+            models.Index(fields=["service", "user"]),
         ]
 
     def save(self, *args, **kwargs):
