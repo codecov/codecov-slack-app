05909d4b2565bf790650bbd45be9666c97371434
Rula Abu Hasna <rula@codecov.io>
2023-05-09 16:02:23 +0300

    update the provider check

--- service_auth/actions.py
+++ service_auth/actions.py
@@ -145,11 +145,11 @@ def handle_codecov_public_api_request(
 
     slack_user = SlackUser.objects.filter(user_id=user_id).first()
     if slack_user.active_service:
-        _service = service if service else slack_user.active_service.name
+        service = slack_user.active_service.name
 
     endpoint_details = get_endpoint_details(
         endpoint_name,
-        service=_service,
+        service=service,
         optional_params=optional_params,
         params_dict=params_dict,
     )
