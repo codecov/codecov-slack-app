e646f99935a895b9d948049f60413568d48887cf
Rula Abu Hasna <rula@codecov.io>
2023-05-09 17:20:00 +0300

    update with github callback tests

--- service_auth/urls.py
+++ service_auth/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import GithubCallbackView
 
 urlpatterns = [
-    path("gh/callback", GithubCallbackView.as_view()),
+    path("gh/callback", GithubCallbackView.as_view(), name="github-callback"),
 ]
