85d9d332bc5d691f09de9c60c72b244ef869a621
Rula Abu Hasna <rula@codecov.io>
2023-06-02 19:28:48 +0300

    update the url

--- service_auth/actions.py
+++ service_auth/actions.py
@@ -64,7 +64,7 @@ def get_or_create_slack_user(user_info):
 
 
 def create_new_codecov_access_token(slack_user: SlackUser):
-    request_url = "http://api.codecov.io/internal/slack/generate-token/"
+    request_url = "https://stage-api.codecov.dev/internal/slack/generate-token/" # make this an env variable
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer {CODECOV_INTERNAL_TOKEN}",
