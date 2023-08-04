d7a5d8940e329b31556120a8c9e4d9446b7e7fc9
Rula Abu Hasna <rula@codecov.io>
2023-08-04 17:42:38 +0300

    resolve FileCoverageReport

--- core/resolvers.py
+++ core/resolvers.py
@@ -585,7 +585,7 @@ class CoverageTrendResolver(BaseResolver):
         return format_nested_keys(data, formatted_data)
 
 
-class FileCoverageReport(BaseResolver):  # Test this
+class FileCoverageReport(BaseResolver):
     """Returns coverage info for a single file specified by path."""
 
     command_name = EndpointName.FILE_COVERAGE_REPORT
@@ -600,7 +600,7 @@ class FileCoverageReport(BaseResolver):  # Test this
 
         repo = params_dict.get("repository")
         path = params_dict.get("path")
-        if data["count"] == 0:
+        if not data:
             return f"No coverage report found for {path} in {repo}"
 
         formatted_data = (
--- service_auth/helpers.py
+++ service_auth/helpers.py
@@ -121,7 +121,7 @@ def get_endpoint_details(
             url=f"{CODECOV_PUBLIC_API}/{service}/{owner_username}/repos/{repository}/compare/components/",
         ),
         EndpointName.FILE_COMPARISON: Endpoint(
-            url=f"{CODECOV_PUBLIC_API}/{service}/{owner_username}/repos/{repository}/compare/file/{file_path}/",
+            url=f"{CODECOV_PUBLIC_API}/{service}/{owner_username}/repos/{repository}/compare/file/{file_path}",
         ),
         EndpointName.FLAG_COMPARISON: Endpoint(
             url=f"{CODECOV_PUBLIC_API}/{service}/{owner_username}/repos/{repository}/compare/flags/",
