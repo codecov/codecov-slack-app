2a7abcc7358bc1df565a70bff2b82611560c5b37
Rula Abu Hasna <rula@codecov.io>
2023-09-07 13:38:16 +0200

    use commit_id or sha but not both

--- core/tests/test__helpers.py
+++ core/tests/test__helpers.py
@@ -146,8 +146,7 @@ def test_format_comparison():
     expected_blocks = [
         DividerBlock(),
         SectionBlock(
-            text="*Head Commit* _1234567_\n"
-            "*ID:* 1234567890abcdef\n"
+            text="*Head Commit* 1234567890abcdef\n"
             "*Branch:* main\n"
             "*Message:* Update README\n"
             "*Author:* John Doe\n"
