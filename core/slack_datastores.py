d5e917a2b87cded64a0d447ab7555fbc1e80890e
Rola <rola.abuhasna@sentry.io>
2024-08-26 11:48:35 +0200

    format and update django

--- core/slack_datastores.py
+++ core/slack_datastores.py
@@ -51,11 +51,11 @@ class DjangoInstallationStore(InstallationStore):
             if is_naive(user_token_expires_at):
                 user_token_expires_at = make_aware(user_token_expires_at)
 
-        # can have one installation per team id 
+        # can have one installation per team id
         slack_installation = SlackInstallation.objects.filter(
             team_id=installation.team_id,
         ).first()
-        
+
         if slack_installation is None:
             slack_installation = SlackInstallation()
 
@@ -94,14 +94,13 @@ class DjangoInstallationStore(InstallationStore):
         )
         slack_installation.token_type = installation.token_type
         slack_installation.installed_at = installed_at
-        
+
         try:
             slack_installation.save()
             self.save_bot(installation.to_bot())
-        
+
         except Exception as e:
             self._logger.error(f"Error saving installation: {e}")
-
 
     def save_bot(self, bot: Bot):
         installed_at = bot.installed_at
